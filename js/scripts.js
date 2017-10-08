3287a7a9f4bda194db8fcfcfe73ba42be2fc8d93
Erik Ring-Walters <erik.ring.walters@gmail.com>
2017-10-07 17:32:49 -0700

    got variable to store in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,19 @@ $(document).ready(function () {
     var wpjs = {
 
         model: null,
-
+        currentDrop: null,
 
 
         init: function () {
           $('#info').hide();
+
+
+          $(".dropbtn").click(function(event){//sets current drop
+            wpjs.currentDrop = this.id;
+            //alert("currentDrop= " + wpjs.currentDrop)
+
+
+          })
 
         },
 
@@ -16,11 +24,7 @@ $(document).ready(function () {
         sampleFunc: function () {
         },
 
-        selectMenu: function() {
-          $('#nabla').click(function() {
-            document.getElementById("drop").classList.toggle("show");
-          })
-        },
+
 
         closeDropdowns: function() {
         $(window).click(function(){
@@ -38,27 +42,48 @@ $(document).ready(function () {
         })
       },
 
-      selectSize: function(){
-        $('#sizeBtn').click(function() {
-          document.getElementById("size").classList.toggle("show");
+      setParentDropDown: function()
+      {
+
+      },
+      // closeSubDropdowns: function(){
+      //   $(window).click(function(
+      //   {if(!event.target.matches(".sub")
+      //   })
+      //   }
+      // },
+      selectMenu: function() {
+        $('#nabla').click(function() {
+          document.getElementById("nablaDrop").classList.toggle("show");
+        })
+      },
+      selectDrop: function(){
+        $("#" + wpjs.currentDrop).click(function() {
+          document.getElementById(wpjs.currentDrop + "Drop").classList.toggle("show");
+        })
+      },
+
+      selectSize: function() {
+        $("#size").click(function() {
+          document.getElementById("sizeDrop").classList.toggle("show");
         })
       },
 
       selectRace: function(){
-        $('#raceBtn').click(function() {
-        document.getElementById("race").classList.toggle("show");
+        $('#race').click(function() {
+        document.getElementById("raceDrop").classList.toggle("show");
       })
     },
 
     selectGender: function(){
-      $('#genderBtn').click(function() {
-        document.getElementById("gender").classList.toggle("show");
+      $('#gender').click(function() {
+        document.getElementById("genderDrop").classList.toggle("show");
       })
     },
 
     selectFeedback: function(){
-      $('#feedbackBtn').click(function(){
-        document.getElementById("feedback").classList.toggle("show");
+      $('#feedback').click(function(){
+        document.getElementById("feedbackDrop").classList.toggle("show");
       })
     },
 
@@ -69,62 +94,19 @@ $(document).ready(function () {
     }
 
       };
-    //
-    //     function selectSize(){
-    //       document.getElementById("size").classList.toggle("show");
-    //     },
-    //     function selectRace(){
-    //       document.getElementById("race").classList.toggle("show");
-    //     },
-    //     function selectGender(){
-    //       document.getElementById("gender").classList.toggle("show");
-    //     },
-    //     function selectFeedback(){
-    //       document.getElementById("feedback").classList.toggle("show");
-    //     },
-    //
-    //     window.onclick = function(event) {
-    //       if(!event.target.matches('.dropbtn'))//if clicked something other than dropdown button
-    //       {
-    //         var dropdowns = document.getElementsByClassName("dropdown-content");
-    //         var i;
-    //         for(i = 0; i < dropdowns.length;i++){
-    //           var openDropdown = dropdowns[i];
-    //           if(openDropdown.classList.contains('show')) {
-    //             openDropdown.classList.remove('show');
-    //           }
-    //         }
-    //       }
-    //     },
-    //
-    //     window.onclick = function(event) {
-    //       if(!event.target.matches('.dropbtn'))//if clicked something other than dropdown button
-    //       {
-    //         var dropdowns = document.getElementsByClassName("dropdown-content");
-    //         var i;
-    //         for(i = 0; i < dropdowns.length;i++){
-    //           var openDropdown = dropdowns[i];
-    //           if(openDropdown.classList.contains('show')) {
-    //             openDropdown.classList.remove('show');
-    //           }
-    //         }
-    //       }
-    //     }
-    //
-    // };
-
-
 
     $(function () {
 
         wpjs.init();
         wpjs.selectMenu();
         wpjs.closeDropdowns();
+        wpjs.selectDrop();
         wpjs.selectSize();
         wpjs.selectRace();
         wpjs.selectGender();
         wpjs.selectFeedback();
         wpjs.showInfo();
+        //wpjs.setCurrentDrop();
     });
 
 
