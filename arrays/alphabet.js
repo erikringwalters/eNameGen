7edd7e7ba568d65ebb8ef80caafb8ac6408c2670
erikringwalters <erik.ring.walters@gmail.com>
2018-01-12 16:36:19 -0800

    pushing alphabet arrays

--- arrays/alphabet.js
+++ arrays/alphabet.js
@@ -2,5 +2,5 @@ var begs = {
   vowel: [ "a", "e", "i", "o", "u" ],
 
   consonant: ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r",
-"s", "t", "v", "w", "x", "y", "z", ]
+"s", "t", "v", "w", "x", "y", "z" ]
 }
