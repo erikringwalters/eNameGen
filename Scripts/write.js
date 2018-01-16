aa8af9bdc223208b98a83697d250e45362350e19
erikringwalters <erik.ring.walters@gmail.com>
2018-01-15 22:38:14 -0800

    updating write

--- Scripts/write.js
+++ Scripts/write.js
@@ -7,12 +7,29 @@ var con = mysql.createConnection({
   database: "enamegen_test"
 });
 
+date(function() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  var today = dd + '/' + mm + '/' + yyyy;
+  return today;
+  document.getElementById("DATE").value = today;
+});
+
 con.likeName(function(err) {
   if (err) throw err;
   console.log("Connected!");
   //Insert a record in the "customers" table:
-  var sql = "INSERT INTO liked (name, size, race, gender, date) VALUES (" + wpjs.name + ", " + wpjs.size + ", " + wpjs.race + ", " + wpjs.gender + ", "Date.now()")";
-  con.query(sql, function (err, result) {
+  var date = date();
+  var sql = "INSERT INTO liked (name, size, race, gender, date) VALUES (" + wpjs.name + ", " + wpjs.size + ", " + wpjs.race + ", " + wpjs.gender + ", " + date + ")";
+  con.query(sql, function(err, result) {
     if (err) throw err;
     console.log("1 record inserted");
   });
@@ -22,8 +39,9 @@ con.dislikeName(function(err) {
   if (err) throw err;
   console.log("Connected!");
   //Insert a record in the "customers" table:
-  var sql = "INSERT INTO disliked (name, size, race, gender, date) VALUES (" + wpjs.name + ", " + wpjs.size + ", " + wpjs.race + ", " + wpjs.gender + ", "Date.now()")";
-  con.query(sql, function (err, result) {
+  var date = date();
+  var sql = "INSERT INTO disliked (name, size, race, gender, date) VALUES (" + wpjs.name + ", " + wpjs.size + ", " + wpjs.race + ", " + wpjs.gender + ", " + date + ")";
+  con.query(sql, function(err, result) {
     if (err) throw err;
     console.log("1 record inserted");
   });
